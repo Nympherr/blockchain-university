9dad4bbab013b432df78d0351160c5455d77e996
Lukas St <114870613+Nympherr@users.noreply.github.com>
2023-09-26 12:16:32 +0300

    Added binary and hex creation methods
    Generates empty(made of 0's) 256-bit binary
    string. Also created a function which converts
    binary string to hexadecimal string.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.math.BigInteger;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Labas pasauli!");
-
+		
+		String binaryString = createEmptyBinaryString();
+		String binaryHex = convertBinaryToHex(binaryString);
+		
+		System.out.println("binary: " + binaryString);
+		System.out.println("hex: " + binaryHex);
+	}
+	
+	public static String createEmptyBinaryString() {
+		
+		int bitLength = 256;
+		StringBuilder emptyBinaryString = new StringBuilder(bitLength);
+		
+		for(int i = 0; i < bitLength; i++) {
+			emptyBinaryString.append("1");
+		}
+		
+		return emptyBinaryString.toString();
+		
+	}
+	
+	public static String convertBinaryToHex(String binaryString) {
+		
+		BigInteger binaryDecimal = new BigInteger(binaryString, 2);
+		return binaryDecimal.toString(16);
 	}
 
 }
